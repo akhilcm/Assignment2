7f896ad02f8b811ec13a2181827a3fb0cbdaf8b5
akhilcm <akhilcm777@gmail.com>
2019-08-16 10:52:24 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Express = require('express');
 var app=new Express();
 app.set('view engine','ejs');
+app.use(Express.static(__dirname+"/public"));
+
 app.get('/',(req,res)=>{
     res.render('addbooks');
 });
@@ -103,6 +105,6 @@ books=[{
 }];
 
 
-app.listen(3005,()=>{
+app.listen(process.env.PORT || 3005,()=>{
     console.log("server running on port 3005");
 });
